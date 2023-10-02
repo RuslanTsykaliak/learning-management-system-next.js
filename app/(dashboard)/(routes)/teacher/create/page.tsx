58d2f4492429231d46c8b4af3948cbb399d8eb4d
Mac <mac@MacBook-Pro.local>
2023-10-02 23:14:38 +0300

    eight

---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import * as z from 'zod'
+import axios from 'axios'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+  FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: 'Title is required',
+  }),
+})
